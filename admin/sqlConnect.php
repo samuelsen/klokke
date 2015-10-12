af562aadab2013a5d7b2982fab8b41fdc3e25fc0
Simen Samuelsen <simendsa@ulrik.uio.no>
2015-10-12 08:48:12 +0200

    Delete sqlConnect.php

--- admin/sqlConnect.php
+++
@@ -1,9 +0,0 @@
-<?
-$username="dbUsername"; 
-$password="dbPassword"; 
-$database="dbDatebase";
-$host="dbHost";
-
-mysql_connect($host,$username,$password);
-@mysql_select_db($database) or die( "Unable to select database");
-?>
